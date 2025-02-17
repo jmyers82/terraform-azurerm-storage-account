e585469ce7d095ac1cad14eadcbe07a6d9551bf8
jeremy myers <rockymtnskier@gmail.com>
2025-02-17 11:16:36 -0700

    add provider

--- providers.tf
+++ providers.tf
@@ -4,12 +4,3 @@
 #   storage_use_azuread             = true
 #   subscription_id                 = var.subscription_id
 # }
-
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 4.0"
-    }
-  }
-}
